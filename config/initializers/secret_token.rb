10ece44ae05dd80b766d505e4dfea4a171e7f73b
Darrell Haas <darrell@lightning-bolt.com>
2014-05-20 09:42:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LbslRailsApp::Application.config.secret_token = '1698c35cc05478b00de81d4e1ef98a64657fd4991063cfd27ffd7c4fcf0dffc8b617e0cc5b313db5e40e2587ddf1fe0ccb7cb5fb239621f07ec65ac31155165b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LbslRailsApp::Application.config.session_store :cookie_store, key: '_lbsl_rails_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LbslRailsApp::Application.config.session_store :active_record_store
